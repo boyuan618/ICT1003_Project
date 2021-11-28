11f78d7d2e1a99e83dc29d0b834dc68ba190330c
Brian Wee <23293463+brian-wee@users.noreply.github.com>
2021-11-28 16:54:57 +0800

    wtv

--- UARTPassThrough/UARTPassThrough.ino
+++ UARTPassThrough/UARTPassThrough.ino
@@ -1,18 +1,7 @@
 //-------------------------------------------------------------------------------
-//  TinyCircuits ST BLE TinyShield UART Example Sketch
-//  Last Updated 2 March 2016
-//
-//  This demo sets up the BlueNRG-MS chipset of the ST BLE module for compatiblity 
-//  with Nordic's virtual UART connection, and can pass data between the Arduino
-//  serial monitor and Nordic nRF UART V2.0 app or another compatible BLE
-//  terminal. This example is written specifically to be fairly code compatible
-//  with the Nordic NRF8001 example, with a replacement UART.ino file with
-//  'aci_loop' and 'BLEsetup' functions to allow easy replacement. 
-//
-//  Written by Ben Rose, TinyCircuits http://tinycircuits.com
-//
+//  From TinyCircuits ST BLE TinyShield UART Example Sketch
+//  Which was written by Ben Rose, TinyCircuits http://tinycircuits.com
 //-------------------------------------------------------------------------------
-
 
 #include <SPI.h>
 #include <STBLE.h>
@@ -42,17 +31,14 @@
 #elif defined(ARDUINO_ARCH_SAMD)
 #define SerialMonitorInterface SerialUSB
 #endif
-
+//for UART logs/debugging
 
 uint8_t ble_rx_buffer[21];
 uint8_t ble_rx_buffer_len = 0;
 uint8_t ble_connection_state = false;
-
 uint8_t amtcolors=7;
 uint8_t colors[]={BLACK,BLUE,RED,GREEN,WHITE,DGREEN,YELLOW};
-
 uint8_t i=0;
-  
 uint8_t nextColor(){
   if(i>=amtcolors)i=0;
   return colors[i++];
@@ -62,13 +48,12 @@ uint8_t nextColor(){
 TinyScreen display = TinyScreen(TinyScreenDefault);
 RTCZero rtc;
 bool clearscreen = false;
-byte buff[100];  //whats this for
+byte buff[100];
 
 void setup() {
-  Wire.begin();//initialize I2C before we can initialize TinyScreen- not needed for TinyScreen+
+  Wire.begin();  //initialize I2C before we can initialize TinyScreen- not needed for TinyScreen+
   display.begin();
-  //setBrightness(brightness);//sets main current level, valid levels are 0-15
-  display.setBrightness(10);
+  display.setBrightness(10);  //setBrightness(brightness);//sets main current level, valid levels are 0-15
   rtc.begin();
   BLEsetup();
 }
@@ -76,38 +61,37 @@ void setup() {
 
 void loop() {
   aci_loop();//Process any ACI commands or events from the NRF8001- main BLE handler, must run often. Keep main loop short.
+  ble_rx_buffer_len = 0;//clear afer reading
   display.clearScreen();
   if (display.getButtons(TSButtonLowerLeft)){ //Update time
     lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)"1", (uint8_t)2);
     delay(1000);
-    Time();
-  } else if (display.getButtons(TSButtonLowerRight)){ //calendar/fun
+    updatetime();
+  } else if (display.getButtons(TSButtonLowerRight)){ //calendar
     lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)"2", (uint8_t)2);
     delay(1000);
     calendar();
   } else if (display.getButtons(TSButtonUpperRight)){ //SOS
     lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)"3", (uint8_t)2);
+    char *text = "Help is on the way!";
+    int width=display.getPrintWidth(text);
+    display.setCursor(48-(width/2),10);    //setCursor(x,y);//set text cursor position to (x,y)
+    display.print(text);
+    delay(1000);
   } else if (display.getButtons(TSButtonUpperLeft)){
-    if (!clearscreen) {
-      clearscreen = true;
-      display.setBrightness(0);
-    } else {
-      clearscreen = false;
-      display.setBrightness(10);
-    }
+    clearscreen = clearscreen^true;
+    if (clearscreen) {display.setBrightness(0);
+    } else {display.setBrightness(10); }
   }
-  if (!clearscreen) {
-    showTime(); //clears screen in fn
-  }
+  if (!clearscreen) {showTime(); } //clears screen in fn 
   delay(1000);
 }
 
-void Time(){
+void updatetime(){
   if (ble_rx_buffer) {
     byte arr[3];
     int arrIndex = 0;
     char *text = (char*)ble_rx_buffer;
-    //char *text = (char*)"133550";
     char hour[3];
     char minute[3];
     char second[3];
@@ -130,8 +114,7 @@ void calendar(){
     display.clearScreen();
     char *text = (char*)ble_rx_buffer;
     int width=display.getPrintWidth(text);
-    //setCursor(x,y);//set text cursor position to (x,y)
-    display.setCursor(48-(width/2),10);
+    display.setCursor(48-(width/2),10);    //setCursor(x,y);//set text cursor position to (x,y)
     display.print(text);
     delay(1000);
   }
@@ -154,92 +137,8 @@ void showTime() {
   strcat(str, snum1);
   strcat(str, " ");
   strcat(str, snum2);
-  //setFont sets a font info header from font.h
-  //information for generating new fonts is included in font.h
-  display.setFont(thinPixel7_10ptFontInfo);
-  //setCursor(x,y);//set text cursor position to (x,y)-
-  display.setCursor(25,32);
-  //fontColor(text color, background color);//sets text and background color
-  display.fontColor(TS_8b_Green,TS_8b_Black);
+  display.setFont(thinPixel7_10ptFontInfo);   //setFont sets a font info header from font.h
+  display.setCursor(25,32);    //setCursor(x,y);//set text cursor position to (x,y)-
+  display.fontColor(TS_8b_Green,TS_8b_Black);  //fontColor(text color, background color);//sets text and background color
   display.print(str);
-}
-
-void fun(){
-  display.setFont(liberationSans_10ptFontInfo);
-  display.fontColor(WHITE,BLACK);
-  
-  for(int i=5;i<20;i++){
-    display.setCursor((3000-(i*5))%70,(i*14)%50);
-    display.print("Hi");
-    display.setCursor((i*7)%70,(6000-(i*6))%50);
-    display.print("Hi");
-    delay(150);
-  }
-  for(int i=0;i<5;i++){
-    display.setCursor(0,i*12);
-    display.print("FASTFASTFASTFAST");
-  }
-  delay(200);
-  
-  for(int i=5;i<70;i++){
-    display.setCursor((3000-(i*5))%70,(i*14)%50);
-    display.fontColor(i|(i<<4),nextColor());
-    display.print("color");
-    display.setCursor((i*7)%70,(6000-(i*6))%50);
-    display.print("COLOR");
-  }
-  display.fontColor(WHITE,BLACK);
-  display.clearWindow(0,0,96,64);
-  display.setFont(liberationSans_8ptFontInfo);
-  display.setCursor(0,0);
-  display.print("SIZE");
-  delay(200);
-  display.setFont(liberationSans_12ptFontInfo);
-  display.setCursor(10,10);
-  display.print("SIZE");
-  delay(200);
-  display.setFont(liberationSans_14ptFontInfo);
-  display.setCursor(24,24);
-  display.print("SIZE");
-  delay(200);
-  display.setFont(liberationSans_16ptFontInfo);
-  display.setCursor(40,40);
-  display.print("SIZE");
-  delay(200);
-  
-  
-  display.setCursor(15,20);
-  display.fontColor(BLACK,WHITE);
-  display.clearWindow(0,0,96,64);
-  display.setBrightness(15);
-  display.drawRect(0,0,96,64,1,WHITE);
-  display.print("BRIGHT!");
-  delay(500);
-  display.fontColor(WHITE,BLACK);
-  display.setCursor(30,20);
-  display.clearWindow(0,0,96,64);
-  display.setBrightness(0);
-  display.print("DIM!");
-  delay(500);
-  display.setBrightness(5);
-  
-  
-  display.fontColor(YELLOW,BLACK);
-  display.setCursor(15,15);
-  display.clearWindow(0,0,96,64);
-  display.print("Boxes!");
-  delay(200);
-  for(int i=5;i<800;i++){
-    int x=(3000-(i*5))%90;
-    int y=(i*14)%60;
-    int width=((i*7)%(90-x));
-    int height=((6000-(i*6))%(60-y));
-    display.drawRect(x,y,constrain(width,5,20),constrain(height,5,15),i&1,nextColor());
-    if(i<10)
-      delay(40);
-    if(i<50)
-      delay(40);
-  }
-  delay(500);
-  display.clearWindow(0,0,96,64);
 }
